f75cdfc57b63ed1a21def54fd0218cf7cc1f9989
Takafumi Kanda <taka-kanda@mail.nissan.co.jp>
2017-05-30 02:16:50 +0900

    Implement Butter Knife sample

---
+++ app/src/main/java/io/github/kafumi/viewbindingsamples/ListAdapter.kt
@@ -0,0 +1,55 @@
+package io.github.kafumi.viewbindingsamples
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
+import java.text.SimpleDateFormat
+import java.util.*
+
+data class Item(val name: String, val time: Long)
+
+class ListAdapter(context: Context, val items: List<Item>) : BaseAdapter() {
+    private val inflater = LayoutInflater.from(context)
+    private val dateFormatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
+
+    override fun getCount() = items.size
+    override fun getItem(position: Int) = items[position]
+    override fun getItemId(position: Int) = position.toLong()
+
+    override fun getView(position: Int, savedView: View?, parent: ViewGroup): View {
+        var view = savedView
+
+        val holder: ViewHolder
+        if (view != null) {
+            holder = view.tag as ViewHolder
+        } else {
+            view = inflater.inflate(android.R.layout.simple_list_item_2, parent, false)
+            holder = ViewHolder(view)
+            view!!.tag = holder
+        }
+
+        val item = getItem(position)
+
+        holder.text1.text = item.name
+        holder.text2.text = dateFormatter.format(Date(item.time))
+
+        return view
+    }
+
+    internal class ViewHolder(view: View) {
+        @BindView(android.R.id.text1)
+        lateinit var text1: TextView
+
+        @BindView(android.R.id.text2)
+        lateinit var text2: TextView
+
+        init {
+            ButterKnife.bind(this, view)
+        }
+    }
+}
--- app/src/main/java/io/github/kafumi/viewbindingsamples/MainActivity.kt
+++ app/src/main/java/io/github/kafumi/viewbindingsamples/MainActivity.kt
@@ -1,11 +1,55 @@
 package io.github.kafumi.viewbindingsamples
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
+import butterknife.OnItemClick
 
 class MainActivity : AppCompatActivity() {
+    @BindView(R.id.items_list)
+    internal lateinit var itemsList: ListView
+
+    @BindView(R.id.list_empty_message)
+    internal lateinit var listEmptyMessage: TextView
+
+    @BindView(R.id.new_item_name)
+    internal lateinit var newItemName: EditText
+
+    private val items = ArrayList<Item>()
+    private lateinit var adapter: ListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+
+        adapter = ListAdapter(this, items)
+
+        itemsList.adapter = adapter
+        itemsList.emptyView = listEmptyMessage
+    }
+
+    @Suppress("unused")
+    @OnItemClick(R.id.items_list)
+    internal fun onItemClick(position: Int) {
+        Toast.makeText(this, "You clicked: " + adapter.getItem(position), Toast.LENGTH_SHORT).show()
+    }
+
+    @Suppress("unused")
+    @OnClick(R.id.add_item)
+    internal fun addItem() {
+        val name = newItemName.text.toString()
+
+        if (name.isNotEmpty()) {
+            newItemName.text.clear()
+            items.add(Item(name, System.currentTimeMillis()))
+            adapter.notifyDataSetChanged()
+        }
     }
 }
