fda633d6a0f8834e259fbce73525ba87fd7cb271
Takafumi Kanda <taka-kanda@mail.nissan.co.jp>
2017-05-30 21:32:18 +0900

    Implement Kotter Knife sample

--- app/src/main/java/io/github/kafumi/viewbindingsamples/MainActivity.kt
+++ app/src/main/java/io/github/kafumi/viewbindingsamples/MainActivity.kt
@@ -1,11 +1,44 @@
 package io.github.kafumi.viewbindingsamples
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.*
+import kotterknife.bindView
 
 class MainActivity : AppCompatActivity() {
+    private val itemsList: ListView by bindView(R.id.items_list)
+    private val listEmptyMessage: TextView by bindView(R.id.list_empty_message)
+    private val newItemName: EditText by bindView(R.id.new_item_name)
+    private val addItem: Button by bindView(R.id.add_item)
+
+    private val items = ArrayList<Item>()
+    private lateinit var adapter: ListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ListAdapter(this, items)
+
+        itemsList.adapter = adapter
+        itemsList.emptyView = listEmptyMessage
+
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        itemsList.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, "You clicked: " + adapter.getItem(position), Toast.LENGTH_SHORT).show()
+        }
+
+        addItem.setOnClickListener {
+            val name = newItemName.text.toString()
+
+            if (name.isNotEmpty()) {
+                newItemName.text.clear()
+                items.add(Item(name, System.currentTimeMillis()))
+                adapter.notifyDataSetChanged()
+            }
+        }
     }
 }
